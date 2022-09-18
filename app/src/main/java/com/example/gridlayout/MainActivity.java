8240467d366b5e5eee15c1410032cd937498cfa2
Isabella <irocha@usc.edu>
2022-09-18 00:27:15 -0700

    Placed bombs randomly, have labeling cells with right number partially working

--- app/src/main/java/com/example/gridlayout/MainActivity.java
+++ app/src/main/java/com/example/gridlayout/MainActivity.java
@@ -11,10 +11,11 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int COLUMN_COUNT = 2;
+    private static final int COLUMN_COUNT = 8;
 
     // save the TextViews of all cells in an array, so later on,
     // when a TextView is clicked, we know which cell it is
@@ -31,45 +32,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cell_tvs = new ArrayList<TextView>();
-
-        // Method (1): add statically created cells
-        TextView tv00 = (TextView) findViewById(R.id.textView00);
-        TextView tv01 = (TextView) findViewById(R.id.textView01);
-        TextView tv10 = (TextView) findViewById(R.id.textView10);
-        TextView tv11 = (TextView) findViewById(R.id.textView11);
-
-        tv00.setTextColor(Color.GRAY);
-        tv00.setBackgroundColor(Color.GRAY);
-        tv00.setOnClickListener(this::onClickTV);
-
-        tv01.setTextColor(Color.GRAY);
-        tv01.setBackgroundColor(Color.GRAY);
-        tv01.setOnClickListener(this::onClickTV);
-
-        tv10.setTextColor(Color.GRAY);
-        tv10.setBackgroundColor(Color.GRAY);
-        tv10.setOnClickListener(this::onClickTV);
-
-        tv11.setTextColor(Color.GRAY);
-        tv11.setBackgroundColor(Color.GRAY);
-        tv11.setOnClickListener(this::onClickTV);
-
-        cell_tvs.add(tv00);
-        cell_tvs.add(tv01);
-        cell_tvs.add(tv10);
-        cell_tvs.add(tv11);
-
         // Method (2): add four dynamically created cells
         GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
-        for (int i = 2; i<=3; i++) {
-            for (int j=0; j<=1; j++) {
+        for (int i = 0; i<=9; i++) {
+            for (int j=0; j<=7; j++) {
                 TextView tv = new TextView(this);
-                tv.setHeight( dpToPixel(64) );
-                tv.setWidth( dpToPixel(64) );
-                tv.setTextSize( 32 );//dpToPixel(32) );
+                tv.setHeight( dpToPixel(32) );
+                tv.setWidth( dpToPixel(32) );
+                tv.setTextSize( 16 );//dpToPixel(32) );
                 tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
-                tv.setTextColor(Color.GRAY);
-                tv.setBackgroundColor(Color.GRAY);
+                tv.setTextColor(Color.GREEN);
+                tv.setBackgroundColor(Color.parseColor("lime"));
                 tv.setOnClickListener(this::onClickTV);
 
                 GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
@@ -82,26 +55,12 @@ public class MainActivity extends AppCompatActivity {
                 cell_tvs.add(tv);
             }
         }
-
-        // Method (3): add four dynamically created cells with LayoutInflater
-        LayoutInflater li = LayoutInflater.from(this);
-        for (int i = 4; i<=5; i++) {
-            for (int j=0; j<=1; j++) {
-                TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
-                //tv.setText(String.valueOf(i)+String.valueOf(j));
-                tv.setTextColor(Color.GRAY);
-                tv.setBackgroundColor(Color.GRAY);
-                tv.setOnClickListener(this::onClickTV);
-
-                GridLayout.LayoutParams lp = (GridLayout.LayoutParams) tv.getLayoutParams();
-                lp.rowSpec = GridLayout.spec(i);
-                lp.columnSpec = GridLayout.spec(j);
-
-                grid.addView(tv, lp);
-
-                cell_tvs.add(tv);
-            }
+        Random rand = new Random();
+        for (int i = 0; i < 4; i++) {
+            int rando = rand.nextInt(80);
+            cell_tvs.get(rando).setText("BOMB");
         }
+
 
     }
 
@@ -113,18 +72,43 @@ public class MainActivity extends AppCompatActivity {
         return -1;
     }
 
+    private int findNumberOfBombs(TextView tv) {
+        int index = findIndexOfCellTextView(tv);
+        int numBombs = 0;
+        for (int indexToCheck = index - 9; indexToCheck < index - 6; indexToCheck++) {
+            if (indexToCheck < 0) {
+                break;
+            }
+            if (cell_tvs.get(indexToCheck).getText().toString().equals("BOMB")) {
+                numBombs += 1;
+            }
+        }
+        if (index % 8 != 0 && cell_tvs.get(index - 1).getText().toString().equals("BOMB")) {
+            numBombs += 1;
+        }
+        if ((index + 1) % 8 != 0 && cell_tvs.get(index + 1).getText().toString().equals("BOMB")) {
+            numBombs += 1;
+        }
+        for (int indexToCheck = index + 7; indexToCheck < index + 10; indexToCheck++) {
+            if (indexToCheck > 70) {
+                break;
+            }
+            if (cell_tvs.get(indexToCheck).getText().toString().equals("BOMB")) {
+                numBombs += 1;
+            }
+        }
+        return numBombs;
+    }
     public void onClickTV(View view){
         TextView tv = (TextView) view;
-        int n = findIndexOfCellTextView(tv);
-        int i = n/COLUMN_COUNT;
-        int j = n%COLUMN_COUNT;
-        tv.setText(String.valueOf(i)+String.valueOf(j));
-        if (tv.getCurrentTextColor() == Color.GRAY) {
-            tv.setTextColor(Color.GREEN);
-            tv.setBackgroundColor(Color.parseColor("lime"));
-        }else {
-            tv.setTextColor(Color.GRAY);
-            tv.setBackgroundColor(Color.LTGRAY);
+        int n = findNumberOfBombs(tv);
+        if (!tv.getText().toString().equals("BOMB") && n != 0) {
+            tv.setText(String.valueOf(n));
         }
+        else if (tv.getText().toString().equals("BOMB")){
+            tv.setText("\uD83D\uDCA3");
+        }
+        tv.setTextColor(Color.GRAY);
+        tv.setBackgroundColor(Color.LTGRAY);
     }
 }
